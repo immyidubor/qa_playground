f32735a2d19605c76243dcc77048c3b7e2b87111
Imuetinyan Idubor <immyidubor@yahoo.com>
2020-12-07 01:40:59 -0500

    Implemented calculator test. Included previous exercises

---
+++ src/practice/creatingFunctions.ts
@@ -0,0 +1,10 @@
+function fancyPrint(firstString: string, secondString: string): string {
+    //code goes here
+    var stringA = firstString;
+    var stringB = secondString;
+    var result = stringA + "\n" + stringB;
+    
+    return result;
+
+}
+console.log(fancyPrint("*****Hello*****", "*****World*****"));
